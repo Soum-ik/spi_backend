f8d11f5eb6a34bb10de421ef2e1cf7de4e1e8df4
Stack Soumik <sarkarsoumik215@gmail.com>
2024-04-12 10:50:59 +0600

    notic api done

--- src/controller/noticController.js
+++ src/controller/noticController.js
@@ -1,2 +1,50 @@
-import notic from "../models/noticModel";
+import Notic from "../models/noticModel.js";
 
+export const get = async (req, res) => {
+  try {
+    const result = await Notic.find();
+    return res.json({ message: "Notic get successfully", data: result });
+  } catch (error) {
+    return res.json({ error: error.message || "Internal Server Error" });
+  }
+};
+export const create = async (req, res) => {
+  try {
+    const reqBody = await req.body;
+    const result = await Notic.create(reqBody);
+    return res.json({ message: "Notic created successfully", data: result });
+  } catch (error) {
+    return res.json({ error: error.message || "Internal Server Error" });
+  }
+};
+export const remove = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await Notic.findByIdAndDelete(id);
+    return res.json({ message: "Notic delete successfully", data: result });
+  } catch (error) {
+    return res.json({ error: error.message || "Internal Server Error" });
+  }
+};
+export const update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updateData = await req.body;
+    const result = await Notic.findByIdAndUpdate({ _id: id }, updateData, {
+      new: true,
+    });
+    return res.json({ message: "Notic updated successfully", data: result });
+  } catch (error) {
+    return res.json({ error: error.message || "Internal Server Error" });
+  }
+};
+export const singleData = async (req, res) => {
+  try {
+    const { name } = req.params;
+
+    const result = await Notic.findOne({ notic_name: name });
+    return res.json({ message: "Notic find  successfully", data: result });
+  } catch (error) {
+    return res.json({ error: error.message || "Internal Server Error" });
+  }
+};
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,10 +1,20 @@
 import express from "express";
 const router = express.Router();
 import * as teacherController from "../controller/teacherController.js";
+import * as noticController from "../controller/noticController.js"; 
 
+// Teacher api
 router.get("/teacher/get", teacherController.get);
 router.post("/teacher/create", teacherController.create);
 router.delete("/teacher/delete/:id", teacherController.remove);
 router.put("/teacher/update/:id", teacherController.update);
 router.patch("/teacher/one/:name", teacherController.singleData);
+
+// Notic api
+router.get("/notic/get", noticController.get);
+router.post("/notic/create", noticController.create);
+router.delete("/notic/delete/:id", noticController.remove);
+router.put("/notic/update/:id", noticController.update);
+router.patch("/notic/one/:name", noticController.singleData);
+
 export default router;

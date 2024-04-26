074bc46ab8785541dbbec7c2e74fd604e1cac7a7
Stack Soumik <sarkarsoumik215@gmail.com>
2024-04-27 01:47:08 +0600

    addd

--- src/models/eventModel.js
+++ src/models/eventModel.js
@@ -4,6 +4,7 @@ const eventSchema = new mongoose.Schema({
   event_name: { type: String, required: true },
   description: { type: String, required: true },
   date: { type: String, required: true },
+  time: { type: String, required: true },
   thumbnail: { type: String, required: true },
   detail: { type: String, required: true },
 });
